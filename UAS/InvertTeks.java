35c5273c6618edf5ab9d41e25342af067638b3b3
gilangpermana10 <gilanghyper27@gmail.com>
2021-07-11 20:52:33 +0700

    menambah invert

--- UAS/InvertTeks.java
+++ UAS/InvertTeks.java
@@ -1,21 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class InvertTeks {
     public static void main(String args[]) {
-        
         String awal, invert = "";
         Scanner input = new Scanner(System.in);
         
+        //input
         System.out.println("Input kata atau kalimat disini : ");
-        
         awal = input.nextLine();
+        input.close();
         
-        for (int i = awal.length() - 1; i >= 0; i--)
-        {
+        //proses invert
+        for (int i = awal.length() - 1; i >= 0; i--){
             invert += awal.charAt(i);
         }
-        
-        System.out.println("Hasil kata yang di balik ; " + invert );
-    
+
+        //output
+        System.out.println("Hasil invert ; " + invert );    
         }
-}
+    }
